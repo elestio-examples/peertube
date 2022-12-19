14a8a5e49689054e2927d039e7552dde9a676c70
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2022-12-19 12:43:52 +0530

    Update postUpdateCommand.sh

--- scripts/postUpdateCommand.sh
+++ scripts/postUpdateCommand.sh
@@ -1,3 +1,4 @@
-docker-compose exec -T -u peertube peertube node dist/scripts/migrations/peertube-5.0.js
 echo "Wait for running"
 sleep 10s;
+docker-compose exec -T -u peertube peertube node dist/scripts/migrations/peertube-5.0.js
+

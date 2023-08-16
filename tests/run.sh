3ec4c0fcb2823cf2ec1e173dd55a9a3fd0f8bb28
Prem Chavhan <pchavhan@logiquad.com>
2023-08-16 14:26:17 +0530

    Updated files

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,7 @@
 sed -i "s~volumes~#volumes~g" ./docker-compose.yml
 sed -i "s~- ./assets:/app/client/dist~#- ./assets:/app/client/dist~g" ./docker-compose.yml
 sed -i "s~- ./data-volume/data:/data~#- ./data-volume/data:/data~g" ./docker-compose.yml
-sed -i "s~- /data-volume/config:/config~#- /data-volume/config:/config~g" ./docker-compose.yml
+sed -i "s~- ./data-volume/config:/config~#- /data-volume/config:/config~g" ./docker-compose.yml
 sed -i "s~- ./data-volume/db:/var/lib/postgresql/data~#- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
 sed -i "s~- ./data-volume/redis:/data~#- ./data-volume/redis:/data~g" ./docker-compose.yml
 docker-compose up -d;
@@ -10,6 +10,6 @@ sleep 45s;
 sed -i "s~#volumes~volumes~g" ./docker-compose.yml
 sed -i "s~#- ./assets:/app/client/dist~- ./assets:/app/client/dist~g" ./docker-compose.yml
 sed -i "s~#- ./data-volume/data:/data~- ./data-volume/data:/data~g" ./docker-compose.yml
-sed -i "s~#- /data-volume/config:/config~- /data-volume/config:/config~g" ./docker-compose.yml
+sed -i "s~#- ./data-volume/config:/config~- /data-volume/config:/config~g" ./docker-compose.yml
 sed -i "s~#- ./data-volume/db:/var/lib/postgresql/data~- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
 sed -i "s~#- ./data-volume/redis:/data~- ./data-volume/redis:/data~g" ./docker-compose.yml

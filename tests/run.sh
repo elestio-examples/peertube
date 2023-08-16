ab8036768e1a9ba7975113aec4891afcc3fd5105
Prem Chavhan <pchavhan@logiquad.com>
2023-08-16 14:11:11 +0530

    Updated files

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-sed -i "s~volumes~#volumes~g" ./Dockerfile
-sed -i "s~- ./assets:/app/client/dist~#- ./assets:/app/client/dist~g" ./Dockerfile
-sed -i "s~- ./data-volume/data:/data~#- ./data-volume/data:/data~g" ./Dockerfile
-sed -i "s~- /data-volume/config:/config~#- /data-volume/config:/config~g" ./Dockerfile
-sed -i "s~- ./data-volume/db:/var/lib/postgresql/data~#- ./data-volume/db:/var/lib/postgresql/data~g" ./Dockerfile
-sed -i "s~- ./data-volume/redis:/data~#- ./data-volume/redis:/data~g" ./Dockerfile
+sed -i "s~volumes~#volumes~g" ./docker-compose.yml
+sed -i "s~- ./assets:/app/client/dist~#- ./assets:/app/client/dist~g" ./docker-compose.yml
+sed -i "s~- ./data-volume/data:/data~#- ./data-volume/data:/data~g" ./docker-compose.yml
+sed -i "s~- /data-volume/config:/config~#- /data-volume/config:/config~g" ./docker-compose.yml
+sed -i "s~- ./data-volume/db:/var/lib/postgresql/data~#- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
+sed -i "s~- ./data-volume/redis:/data~#- ./data-volume/redis:/data~g" ./docker-compose.yml
 docker-compose up -d;
 sleep 45s;
-sed -i "s~#volumes~volumes~g" ./Dockerfile
-sed -i "s~#- ./assets:/app/client/dist~- ./assets:/app/client/dist~g" ./Dockerfile
-sed -i "s~#- ./data-volume/data:/data~- ./data-volume/data:/data~g" ./Dockerfile
-sed -i "s~#- /data-volume/config:/config~- /data-volume/config:/config~g" ./Dockerfile
-sed -i "s~#- ./data-volume/db:/var/lib/postgresql/data~- ./data-volume/db:/var/lib/postgresql/data~g" ./Dockerfile
-sed -i "s~#- ./data-volume/redis:/data~- ./data-volume/redis:/data~g" ./Dockerfile
+sed -i "s~#volumes~volumes~g" ./docker-compose.yml
+sed -i "s~#- ./assets:/app/client/dist~- ./assets:/app/client/dist~g" ./docker-compose.yml
+sed -i "s~#- ./data-volume/data:/data~- ./data-volume/data:/data~g" ./docker-compose.yml
+sed -i "s~#- /data-volume/config:/config~- /data-volume/config:/config~g" ./docker-compose.yml
+sed -i "s~#- ./data-volume/db:/var/lib/postgresql/data~- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
+sed -i "s~#- ./data-volume/redis:/data~- ./data-volume/redis:/data~g" ./docker-compose.yml

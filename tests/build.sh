4067be8bb6423bb3e162383f1dd65cb55f3e5ac8
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-12 19:16:04 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,6 @@
 cp -rf support/docker/production/* ./
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
-mv Dockerfile.bullseye Dockerfile
+mv Dockerfile.bookworm Dockerfile
 
 docker buildx build . --output type=docker,name=elestio4test/peertube:latest | docker load

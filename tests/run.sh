ae101d314b177c59f4b558a44f4f20d7f711797d
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-12 19:58:49 +0530

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
-sed -i "s~volumes~#volumes~g" ./docker-compose.yml
-sed -i "s~- ./assets:/app/client/dist~#- ./assets:/app/client/dist~g" ./docker-compose.yml
-sed -i "s~- ./data-volume/data:/data~#- ./data-volume/data:/data~g" ./docker-compose.yml
-sed -i "s~- ./data-volume/config:/config~#- ./data-volume/config:/config~g" ./docker-compose.yml
-sed -i "s~- ./data-volume/db:/var/lib/postgresql/data~#- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
-sed -i "s~- ./data-volume/redis:/data~#- ./data-volume/redis:/data~g" ./docker-compose.yml
 docker-compose up -d;
 sleep 300s;
-sed -i "s~#volumes~volumes~g" ./docker-compose.yml
-sed -i "s~#- ./assets:/app/client/dist~- ./assets:/app/client/dist~g" ./docker-compose.yml
-sed -i "s~#- ./data-volume/data:/data~- ./data-volume/data:/data~g" ./docker-compose.yml
-sed -i "s~#- ./data-volume/config:/config~- ./data-volume/config:/config~g" ./docker-compose.yml
-sed -i "s~#- ./data-volume/db:/var/lib/postgresql/data~- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
-sed -i "s~#- ./data-volume/redis:/data~- ./data-volume/redis:/data~g" ./docker-compose.yml

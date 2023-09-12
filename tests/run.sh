c83d07a02159d330f34f3bc1278b87083f4df00b
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-12 19:46:01 +0530

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -6,7 +6,7 @@ sed -i "s~- ./data-volume/config:/config~#- ./data-volume/config:/config~g" ./do
 sed -i "s~- ./data-volume/db:/var/lib/postgresql/data~#- ./data-volume/db:/var/lib/postgresql/data~g" ./docker-compose.yml
 sed -i "s~- ./data-volume/redis:/data~#- ./data-volume/redis:/data~g" ./docker-compose.yml
 docker-compose up -d;
-sleep 45s;
+sleep 300s;
 sed -i "s~#volumes~volumes~g" ./docker-compose.yml
 sed -i "s~#- ./assets:/app/client/dist~- ./assets:/app/client/dist~g" ./docker-compose.yml
 sed -i "s~#- ./data-volume/data:/data~- ./data-volume/data:/data~g" ./docker-compose.yml
